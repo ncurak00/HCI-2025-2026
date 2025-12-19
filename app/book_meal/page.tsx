d3ee0fcf92a13f8c4d6b02cbdbe922b3d64cb4bd
ncurak00 <115464211+ncurak00@users.noreply.github.com>
2025-12-19 17:43:17 +0100

    Delete app directory

--- app/book_meal/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-10">
-      <h1 className="text-6xl font-extrabold tracking-tight">Book a meal</h1>
-    </main>
-  );
-}
